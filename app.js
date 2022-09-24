768aca851074f1194c6d2d1c4eafc46a603e0c62
Balasubramanian <61572479+companyofbala@users.noreply.github.com>
2022-09-25 00:20:40 +0530

    d

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const session = require("express-session");
 
 const app = express();
 const userRouter = require("./api/users/user.router");
@@ -10,26 +6,6 @@ const movieRouter = require("./api/movies/movies.router");
 require("dotenv").config();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    methods: ["GET", "POST", "PATCH", "DELETE"],
-    credentials: true,
-  })
-);
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  session({
-    key: "userId",
-    secret: "secretkey",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      expires: 60 * 60,
-    },
-  })
-);
 
 app.use("/api/users", userRouter);
 app.use("/api/movies", movieRouter);
